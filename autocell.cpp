33c08e1e949b8be592ac6b23f23c9eb634415868
Aghiles HAMACHE <aghiles.hamache@etu.utc.fr>
2018-06-03 22:15:35 +0200

    ajout Automate1D, Etat1D et Simulateur1D

--- autocell.cpp
+++ autocell.cpp
@@ -2,10 +2,148 @@
 
 using namespace std;
 
-Etat2D::Etat2D(unsigned int nbL, unsigned int nbC): nbLigne(nbL), nbColonne(nbC),
- valeurs(nullptr){
+Etat1D::Etat1D(unsigned int n):nbColonnes(n),
+	valeur(new bool [n]) {
+	for (unsigned int i = 0; i < n; i++) valeur[i] = false;
+}
 
-    if (nbC==0 && nbL==0)
+void Etat1D::setCellule(unsigned int i, bool val) {
+	if (i >= nbColonnes) throw AutomateException("Erreur Cellule");
+	valeur[i] = val;
+}
+
+bool Etat1D::getCellule(unsigned int i) const {
+	if (i >= nbColonnes) throw AutomateException("Erreur Cellule");
+	return valeur[i];
+}
+
+Etat1D::Etat1D(const Etat1D& e):nbColonnes(e.nbColonnes),valeur(new bool[e.nbColonnes]){
+	for (unsigned int i = 0; i < nbColonnes; i++) valeur[i] = e.valeur[i];
+}
+
+std::ostream& operator<<(std::ostream& f, const Etat1D& e) {
+	for (unsigned int i = 0; i < e.getNbColonnes(); i++)
+		if (e.getCellule(i)) f << char(178); else f << " ";
+	return f;
+}
+
+Etat1D& Etat1D::operator=(const Etat1D& e) {
+	if (this != &e) {
+		if (nbColonnes != e.nbColonnes) {
+			bool* newvaleur = new bool[e.nbColonnes];
+			for (unsigned int i = 0; i < e.nbColonnes; i++) newvaleur[i] = e.valeur[i];
+			bool* old = valeur;
+			valeur = newvaleur;
+			nbColonnes = e.nbColonnes;
+			delete[] old;
+		}else for (unsigned int i = 0; i < e.nbColonnes; i++) valeur[i] = e.valeur[i];
+	}
+	return *this;
+}
+
+short unsigned int NumBitToNum(const std::string& num) {
+	if (num.size() != 8) throw AutomateException("Numero d'automate indefini");
+	int puissance = 1;
+	short unsigned int numero = 0;
+	for (int i = 7; i >= 0; i--) {
+		if (num[i] == '1') numero += puissance;
+		else if (num[i] != '0') throw AutomateException("Numero d'automate indefini");
+		puissance *= 2;
+	}
+	return numero;
+}
+
+std::string NumToNumBit(short unsigned int num) {
+	std::string numeroBit;
+	if (num > 256) throw AutomateException("Numero d'automate indefini");
+	unsigned short int p = 128;
+	int i = 7;
+	while (i >= 0) {
+		if (num >= p) {
+			numeroBit.push_back('1');
+			num -= p;
+		}
+		else { numeroBit.push_back('0'); }
+		i--;
+		p = p / 2;
+	}
+	return numeroBit;
+}
+
+Automate1D::Automate1D(unsigned short int num):numero(num),numeroBit(NumToNumBit(num)){
+}
+
+Automate1D::Automate1D(const std::string& num) :numero(NumBitToNum(num)),numeroBit(num) {
+}
+
+
+
+void Automate1D::appliquerTransition(const Etat1D& dep, Etat1D& dest) const {
+	if (dep.getNbColonnes() != dest.getNbColonnes()) dest = dep;
+	for (unsigned int i = 0; i < dep.getNbColonnes(); i++) {
+		unsigned short int conf=0;
+		if (i > 0) conf+=dep.getCellule(i - 1) * 4;
+		conf+=dep.getCellule(i)*2;
+		if (i < dep.getNbColonnes()-1) conf+=dep.getCellule(i + 1);
+		dest.setCellule(i, numeroBit[7-conf]-'0');
+	}
+}
+
+std::ostream& operator<<(std::ostream& f, const Automate1D& A) {
+	f << A.getNumero() << " : " << A.getNumeroBit() << "\n";
+	return f;
+}
+
+Simulateur1D::Simulateur1D(const Automate1D& a, unsigned int buffer): automate(a), etats(nullptr), depart(nullptr), nbMaxEtats(buffer),rang(0) {
+	etats = new Etat1D*[nbMaxEtats];
+	for (unsigned int i = 0; i < nbMaxEtats; i++) etats[i] = nullptr;
+}
+
+Simulateur1D::Simulateur1D(const Automate1D& a, const Etat1D& dep, unsigned int buffer):automate(a), etats(nullptr), depart(&dep), nbMaxEtats(buffer),rang(0) {
+	etats = new Etat1D*[nbMaxEtats];
+	for (unsigned int i = 0; i < nbMaxEtats; i++) etats[i] = nullptr;
+	etats[0] = new Etat1D(dep);
+}
+
+void Simulateur1D::build(unsigned int cellule) {
+	if (cellule >= nbMaxEtats) throw AutomateException("erreur taille buffer");
+	if (etats[cellule] == nullptr) etats[cellule] = new Etat1D;
+}
+
+void Simulateur1D::setEtatDepart(const Etat1D& e) {
+	depart = &e;
+	reset();
+}
+
+void Simulateur1D::reset() {
+	if (depart==nullptr) throw AutomateException("etat depart indefini");
+	build(0); *etats[0] = *depart;
+	rang = 0;
+}
+
+void Simulateur1D::next() {
+	if (depart == nullptr) throw AutomateException("etat depart indefini");
+	rang++;
+	build(rang%nbMaxEtats);
+	automate.appliquerTransition(*etats[(rang - 1) % nbMaxEtats], *etats[rang%nbMaxEtats]);
+}
+
+void Simulateur1D::run(unsigned int nb_steps) {
+	for (unsigned int i = 0; i < nb_steps; i++) next();
+}
+
+const Etat1D& Simulateur1D::dernier() const {
+	return *etats[rang%nbMaxEtats];
+}
+
+Simulateur1D::~Simulateur1D() {
+	for (unsigned int i = 0; i < nbMaxEtats; i++) delete etats[i];
+	delete[] etats;
+}
+
+Etat2D::Etat2D(unsigned int nbL, unsigned int nbC): nbLigne(nbL), nbColonne(nbC),valeurs(nullptr){
+
+    if (nbC==0 || nbL==0) // && ou || ?
         throw AutomateException("Mauvaise dimension de la matrice");
 
     else{
@@ -18,15 +156,13 @@ Etat2D::Etat2D(unsigned int nbL, unsigned int nbC): nbLigne(nbL), nbColonne(nbC)
     }
 }
 
-bool Etat2D::getCellule(unsigned int i, unsigned int j) const
-{
+bool Etat2D::getCellule(unsigned int i, unsigned int j) const{
     if (i>=nbLigne|| j>=nbColonne)
         throw AutomateException("Cellule inexistante");
     return valeurs[i][j];
 }
 
-void Etat2D::setCellule(unsigned int i, unsigned int j, bool val)
-{
+void Etat2D::setCellule(unsigned int i, unsigned int j, bool val){
     if (i>=nbLigne || j>=nbColonne)
         throw AutomateException("Cellule inexistante");
     valeurs[i][j]=val;
@@ -66,10 +202,9 @@ std::ostream& operator<<(std::ostream& f, const Etat2D& e){
 }
 
 
-Etat2D::Etat2D(const Etat2D& e) : nbLigne(e.nbLigne),
-    nbColonne(e.nbColonne), valeurs(nullptr){
+Etat2D::Etat2D(const Etat2D& e) : nbLigne(e.nbLigne),nbColonne(e.nbColonne), valeurs(nullptr){
 
-    if(nbLigne<=0 || nbColonne<=0)
+    if(nbLigne<=0 || nbColonne<=0)      //impossible dans tous les cas ! cf. le constructeur de Etat2D. Du coup si vous petes ok on supprime ce test
         throw AutomateException("Mauvaise dimension");
     else {
         valeurs = new bool*[nbLigne];
@@ -84,7 +219,7 @@ Etat2D::Etat2D(const Etat2D& e) : nbLigne(e.nbLigne),
 
 Etat2D& Etat2D::operator=(const Etat2D& e){
 
-    if(this != &e){ //auto-affectation inutile
+    if(this != &e){ //auto-affectation impossible
         if(nbLigne!=e.nbLigne){ //on refait tout le tableau car pas bon nombre de ligne dès le début
 
             for(unsigned int i=0;i<nbLigne;++i)
@@ -170,16 +305,14 @@ void Simulateur2D::setEtatDepart(const Etat2D& e){
 
 }
 
-Simulateur2D::Simulateur2D(const Automate2D& a, unsigned int buf):
-    automate(a), depart(nullptr), nbMaxEtats(buf), rang(0){
+Simulateur2D::Simulateur2D(const Automate2D& a, unsigned int buf):automate(a), depart(nullptr), nbMaxEtats(buf), rang(0){
 
         etats=new Etat2D*[nbMaxEtats];
         for(unsigned int i = 0; i<nbMaxEtats; i++)
             etats[i]=nullptr;
 }
 
-Simulateur2D::Simulateur2D(const Automate2D& a, const Etat2D& dep, unsigned int buffer):
-    automate(a), depart(&dep), nbMaxEtats(buffer), rang(0){
+Simulateur2D::Simulateur2D(const Automate2D& a, const Etat2D& dep, unsigned int buffer):automate(a), depart(&dep), nbMaxEtats(buffer), rang(0){
 
         etats= new Etat2D*[nbMaxEtats];
         for(unsigned int i=0; i<nbMaxEtats; i++)
--- main.cpp
+++ main.cpp
@@ -5,17 +5,40 @@ using namespace std;
 
 int main()
 {
-    Automate2D a(2,3,2,4);
+    /*Automate2D a(2,3,2,4);
 
     cout << "Creation d'etats" << endl;
-    Etat2D e1(7,4);
-    e1.setCellule(4,2,true);
-    e1.setCellule(3,1,true);
-    e1.setCellule(6,3,true);
-    e1.setCellule(1,0,true);
-    e1.setCellule(6,0,true);
+    Etat2D e2(7,4);
+    e2.setCellule(4,2,true);
+    e2.setCellule(3,1,true);
+    e2.setCellule(6,3,true);
+    e2.setCellule(1,0,true);
+    e2setCellule(6,0,true);
 
-    Simulateur2D s(a,e1);
+    Simulateur2D s(a,e2);
+    cout<<e2;
+    for(unsigned int i=0;i<10;i++){
+
+        s.next();
+        cout<<s.dernier()<<"\n";
+
+    }*/
+
+    cout << "Creation d'etats" << endl;
+    Etat1D e1(15);
+    e1.setCellule(4,true);
+    e1.setCellule(3,true);
+    e1.setCellule(6,true);
+    e1.setCellule(1,true);
+    e1.setCellule(13,true);
+    e1.setCellule(11,true);
+    e1.setCellule(10,true);
+    e1.setCellule(0,true);
+    e1.setCellule(8,true);
+
+    Automate1D a=Automate1D(25);
+
+    Simulateur1D s(a,e1);
     cout<<e1;
     for(unsigned int i=0;i<10;i++){
 
@@ -23,6 +46,7 @@ int main()
         cout<<s.dernier()<<"\n";
 
     }
+
 
     return 0;
 }

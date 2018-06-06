8f685b8a603709a22f0203da95737818d905b7f2
Segolene <sego.b@hotmail.fr>
2018-06-06 19:14:33 +0200

    heritage + pb Aut2D

--- automate2D.h
+++ automate2D.h
@@ -3,63 +3,25 @@
 
 #include "autocell.h"
 
-class Etat2D : public Etat { //grille de l'application
-
-    unsigned int nbLigne;
-    valeurs* grille;
-
-public:
-    Etat2D() : nbLigne(0), nbColonne(0), valeurs(nullptr) {}
-    Etat2D(unsigned int nbL, unsigned int nbC);
-    const unsigned int getCol()const {return nbColonne;}
-    const unsigned int getRow()const {return nbLigne;}
-    bool getCellule(unsigned int i, unsigned int j) const;
-    void setCellule(unsigned int i, unsigned int j, bool val);
-    ~Etat2D();
-    Etat2D(const Etat2D& e);
-    Etat2D& operator=(const Etat2D& e);
-    unsigned int CountVoisin(unsigned int li, unsigned int co) const;
-};
-
-std::ostream& operator<<(std::ostream& f, const Etat2D& e);
-
 class Automate2D : public Automate{
 
-    unsigned int nbMinVivant; /*sachant une cellule vivante, cb il
-    faut de cellule vivante minimum pour qu'elle vive à t+1*/
+    unsigned int nbMinVivant; /*sachant une cellule vivante, cb il faut de cellule vivante minimum pour qu'elle vive à t+1*/
     unsigned int nbMaxVivant;
     unsigned int nbMinMort; /*même choix pour une cellule morte*/
     unsigned int nbMaxMort;
 
 public :
-    Automate2D(unsigned int minV=0, unsigned int maxV=0, unsigned int minM=0, unsigned int maxM=0);
+    Automate2D(unsigned int minV=0, unsigned int maxV=0, unsigned int minM=0, unsigned int maxM=0, unsigned int n=0);
     const unsigned int getMinV()const{return nbMinVivant;}
     const unsigned int getMaxV()const{return nbMaxVivant;}
     const unsigned int getMinM()const{return nbMinMort;}
     const unsigned int getMaxM()const{return nbMaxMort;}
     ~Automate2D()=default;
-    void AppliquerTransition(const Etat2D& dep, Etat2D& dest) const;
+    void AppliquerTransition(const Etat& dep, Etat& dest) const;
+    unsigned int CountVoisin(unsigned int li, unsigned int co, const Etat& e) const;
 };
 
-class Simulateur2D : public Simulateur{
+std::ostream& operator<<(std::ostream& f, const Automate2D& A);
 
-    const Automate2D& automate;
-    const Etat2D* depart=nullptr;
-    Etat2D** etats=nullptr;
-    unsigned int nbMaxEtats; //nb max qu'on sauvegarde dans le tableau etats
-    unsigned int rang=0;
-
-public :
-    void setEtatDepart(const Etat2D& e);
-    Simulateur2D(const Automate2D& a, unsigned int buf=2);
-    Simulateur2D(const Automate2D& a, const Etat2D& dep, unsigned int buffer=2);
-    void next();
-    void run(unsigned int nbSteps);
-    void reset();
-    const Etat2D& dernier() const;
-    unsigned int getRangDernier()const;
-    ~Simulateur2D();
-
-};
 
 #endif // AUTOMATE2D_H_INCLUDED
